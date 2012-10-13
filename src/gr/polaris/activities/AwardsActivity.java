c240428f9e4ef844c5a77f5aa4c034f13a92b4a7
MartinMikita <martin@mikita.eu>
2012-10-13 18:25:29 +0200

    FIXED unused package name blbecci ADDED activity awards layout xml ADDED base methods for Awards Activity ADDED abstract BaseActivity ADDED Awards Activity to Manifest

--- src/gr/polaris/activities/AwardsActivity.java
+++ src/gr/polaris/activities/AwardsActivity.java
@@ -1,5 +1,13 @@
 package gr.polaris.activities;
 
-public class AwardsActivity {
+import gr.polaris.R;
+import android.os.Bundle;
 
+public class AwardsActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_awards);
+    }
 }
--- src/gr/polaris/activities/RoomsActivity.java
+++ src/gr/polaris/activities/RoomsActivity.java
@@ -1,23 +1,17 @@
 package gr.polaris.activities;
 
-import gr.polaris.blbecci.R;
-import gr.polaris.blbecci.R.layout;
-import gr.polaris.blbecci.R.menu;
+import gr.polaris.R;
+import gr.polaris.R.layout;
+import gr.polaris.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class RoomsActivity extends Activity {
+public class RoomsActivity extends BaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.all_activities, menu);
-        return true;
     }
 }
